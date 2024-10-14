9ef556add1addbbee2a1b55a45fcfa6441e2c12e
vincentmpalma <vincentmichaelpalma@gmail.com>
2024-10-14 12:13:09 -0700

    Added endpoints for getting items for specific wishlists and to get all wishlists by user and some error handling

--- src/main/java/com/csumb/WishlistBackendDB/services/ItemService.java
+++ src/main/java/com/csumb/WishlistBackendDB/services/ItemService.java
@@ -5,8 +5,9 @@ import com.csumb.WishlistBackendDB.models.Item;
 import java.util.List;
 
 public interface ItemService {
-    public Item addItem(Item item);
+    public boolean addItem(Item item);
     public List<Item> getAllItems();
-    public void deleteItem(int itemID);
+    public int deleteItem(int itemID);
     public Item getItem(int itemID);
+    public int editItem(Item item);
 }
